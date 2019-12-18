569278b7391bb654ca078cc5325e7991d43f36fe
Mirosław Malik <mmiro390@gmail.com>
2019-12-18 12:14:02 +0100

    add glass counter and localStorage

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,11 @@
 // service worker registration - remove if you're not going to use it
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('serviceworker.js').then(function (registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
+    }, function (err) {
       // registration failed :(
       console.log('ServiceWorker registration failed: ', err);
     });
@@ -17,6 +17,31 @@ if ('serviceWorker' in navigator) {
 // place your code below
 
 
-console.log(`Hello world!`);
+// console.log(`Hello world!`);
+
+const spanCounter = document.querySelector('.glass__counter--js');
+const addButton = document.querySelector('.container__add-glass--js');
+const delButton = document.querySelector('.container__del-glass--js');
 
 
+let counter = localStorage.getItem('glassNumber');
+spanCounter.innerHTML = localStorage.getItem('glassNumber');
+
+addButton.addEventListener('click', () => {
+  // spanCounter.innerHTML = parseInt(spanCounter.innerHTML)+1; // you can do something like this
+  counter++;
+  localStorage.setItem('glassNumber', counter);
+  // spanCounter.innerHTML = counter;
+  spanCounter.innerHTML = localStorage.getItem('glassNumber');
+});
+
+delButton.addEventListener('click', () => {
+  if (counter <= 0) {
+    counter = 0;
+  } else {
+    counter--;
+    localStorage.setItem('glassNumber', counter);
+    spanCounter.innerHTML = localStorage.getItem('glassNumber');
+  }
+  spanCounter.innerHTML = counter;
+});
